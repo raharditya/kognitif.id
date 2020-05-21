6c4446c019cf6beb0d7cb115e126afd05eeabc26
Asebodi <aditraharditya45@gmail.com>
2020-05-21 22:15:25 +0700

    Implemented animation with gsap and css

--- src/Content.js
+++ src/Content.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
+import { TweenMax, Power3, TimelineLite } from "gsap";
+import { Timeline, Tween } from "react-gsap";
 
 import { ReactComponent as Center } from "./assets/center.svg";
 import { ReactComponent as Phone } from "./assets/phone.svg";
@@ -10,46 +12,109 @@ import { ReactComponent as Laptop } from "./assets/laptop.svg";
 import { ReactComponent as BottomPaper } from "./assets/bottom-paper.svg";
 import { ReactComponent as Water3 } from "./assets/water-3.svg";
 import { ReactComponent as Rss } from "./assets/rss.svg";
+import { TweenLite } from "gsap/gsap-core";
 
 export default function Content() {
+  let mainTitle = useRef(null);
+
+  let navLine = useRef(null);
+  let navYear = useRef(null);
+
+  let line1 = useRef(null);
+  let line2 = useRef(null);
+  let line3 = useRef(null);
+
+  let graphic = useRef(null);
+
+  useEffect(() => {
+    TweenLite.staggerFrom(
+      [navLine, navYear],
+      1,
+      {
+        scaleX: 0,
+        xPercent: -60,
+        ease: Power3.easeOut,
+        delay: 0.5,
+      },
+      0.2
+    );
+    TweenMax.from(mainTitle, 2, {
+      opacity: 0,
+      yPercent: 20,
+      ease: Power3.easeOut,
+    });
+    TweenMax.staggerFrom(
+      [line1, line2, line3],
+      1.5,
+      {
+        yPercent: 100,
+        ease: Power3.easeOut,
+        delay: 1,
+      },
+      0.2
+    );
+  }, []);
   return (
     <div>
       <nav className="main-wrapper">
-        <div className="line"></div>
-        <p>2020</p>
+        <div className="line" ref={(el) => (navLine = el)}></div>
+        <p ref={(el) => (navYear = el)}>2020</p>
       </nav>
 
       <div class="desktop-flex">
         <header className="main-wrapper">
-          <h1 className="main-title">Kognitif.id</h1>
-          <h2>
-            Making Your
-            <br />
-            Next
-            <span className="emphasis-text">
-              &nbsp;Awesome
-              <br />
-              Website
-            </span>
-          </h2>
+          <h1 className="main-title" ref={(el) => (mainTitle = el)}>
+            Kognitif.id
+          </h1>
+          <div className="hero-text">
+            <h2>
+              <span className="text-show" ref={(el) => (line1 = el)}>
+                Making Your
+              </span>
+            </h2>
+            <h2>
+              <span className="text-show" ref={(el) => (line2 = el)}>
+                Next
+                <span className="emphasis-text">&nbsp;Awesome</span>
+              </span>
+            </h2>
+            <h2>
+              <span
+                className="text-show emphasis-text"
+                ref={(el) => (line3 = el)}
+              >
+                Websites.
+              </span>
+            </h2>
+          </div>
         </header>
 
-        <section>
-          <Center className="img-center" />
-          <Phone className="img-phone" />
-          <Www className="img-www" />
-          <Water1 className="img-water-1" />
-          <TopPaper className="img-top-paper" />
-          <Water2 className="img-water-2" />
-          <Laptop className="img-laptop" />
-          <BottomPaper className="img-bottom-paper" />
-          <Water3 className="img-water-3" />
-          <Rss className="img-rss" />
+        <section ref={(el) => (graphic = el)}>
+          <Tween
+            from={{ y: "-20%", opacity: 0 }}
+            duration={4}
+            stagger={0.2}
+            delay={2}
+            ease="elastic.out(0.2, 0.1)"
+          >
+            <Center className="img-center" />
+            <Www className="img-www" />
+            <Water1 className="img-water-1" />
+            <Laptop className="img-laptop" />
+            <Phone className="img-phone" />
+            <TopPaper className="img-top-paper" />
+            <BottomPaper className="img-bottom-paper" />
+            <Water2 className="img-water-2" />
+            <Rss className="img-rss" />
+            <Water3 className="img-water-3" />
+          </Tween>
         </section>
       </div>
 
       <footer>
-        <p>Coming Soon</p>
+        <Tween from={{ opacity: 0 }} duration={1} delay={5}>
+          <p>Coming Soon</p>
+        </Tween>
       </footer>
     </div>
   );
