8a04ba9e6f60882b393539f9107e2210046d2293
frostyde <coc.dwiki@gmail.com>
2021-04-15 12:25:49 +0700

    create page heading

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,7 +8,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header></header>
+      <header>
+        <div className="">
+          <div className="">
+            <h2 className="">KOGNITIF WEB DEVELOPMENT</h2>
+            <h1 className="main-title">Bawa Bisnismu ke Era Modern</h1>
+            <p>
+            We’ve got a vast code base of modules and ready-made solutions
+  that speed up the development process and cut your expenses.
+            </p>
+            <button> View Portofolio </button>
+            <button> Hire Us! </button>
+          </div>
+          <img src="" alt=""/>
+        </div>
+      </header>
 
       <section></section>
     </div>
