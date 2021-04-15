3bba2327f61195052a76779860eae878f734527b
Asebodi <aditraharditya45@gmail.com>
2021-04-15 12:00:44 +0700

    Initial setup

---
+++ pages/index.jsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Kognitif - Jasa Pembuatan Website</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header></header>
+
+      <section></section>
+    </div>
+  );
+}
