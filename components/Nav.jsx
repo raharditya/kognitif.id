58215bfb934efc4da7f17eaf6f7acd3fa90ecc4d
Asebodi <aditraharditya45@gmail.com>
2021-04-17 13:43:35 +0700

    Added nav

---
+++ components/Nav.jsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import React from "react";
+
+function Nav(props) {
+  return (
+    <nav>
+      <div className="inner-nav">
+        <img src="/svg/logo.svg" alt="Logo Kognitif" />
+
+        <div className="">
+          <ul>
+            <li>
+              <Link href="/">
+                <a>Beranda</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>Portofolio</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>FAQ</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>Tentang Kami</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>Kontak</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a className="heading-bold btn-primary nav-btn">PESAN</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import Nav from "../components/Nav";
 import "../styles/globals.scss";
+import "../styles/nav.scss";
+import "../styles/footer.scss";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Nav />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
 export default MyApp;
