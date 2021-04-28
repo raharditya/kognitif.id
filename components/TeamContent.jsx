64f54283892afc6122f7918627984fafd937bb3a
frostyde <coc.dwiki@gmail.com>
2021-04-28 08:04:57 +0700

    change about us property and layout

--- components/TeamContent.jsx
+++ components/TeamContent.jsx
@@ -5,7 +5,7 @@ function TeamContent(props) {
         <div className="tc-flex">
             <div className="tc-container">
                 <div className="tc-info">
-                    <div className="tc-image"></div>
+                    <div className="tc-image" style={{backgroundImage: `url("/profile-pic.png")`}}></div>
                     <div className="tc-desc">
                         <h2> Lorema Obelix </h2>
                         <p> Programmer </p>
